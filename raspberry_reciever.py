bb8316e6cba19c668330c56731f7896bd21fdafa
rov4lyf <rov4lyf@gmail.com>
2026-01-10 12:40:30 -0800

    Add files via upload

---
+++ raspberry_reciever.py
@@ -0,0 +1,18 @@
+
+import socket
+
+HOST = '0.0.0.0'  # Standard loopback interface address (localhost)
+PORT = 60000        # Port to listen on (non-privileged ports are > 1023)
+print("debug")
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(4096)
+            if len(data) > 0:
+                print("debug2")
+                print(data)
